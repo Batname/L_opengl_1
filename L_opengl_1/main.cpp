d553a52560d34c7a094424c7010ee05a25ebb604
DenisDubinin <dadubinin@gmail.com>
2016-12-15 12:50:02 +0100

    Initial Commit

---
+++ L_opengl_1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  L_opengl_1
+//
+//  Created by apple on 2016-12-15.
+//  Copyright © 2016 apple. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
