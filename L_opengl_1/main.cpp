6af70a4e4d1ab6e553eb2da8f91ca2fd0e792f1b
DenisDubinin <dadubinin@gmail.com>
2016-12-15 14:39:35 +0100

    clear color

--- L_opengl_1/main.cpp
+++ L_opengl_1/main.cpp
@@ -41,7 +41,13 @@ int main(int argc, const char * argv[]) {
     
     // game loop
     while (!glfwWindowShouldClose(window)) {
+        // check evants
         glfwPollEvents();
+        
+        // render here
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
         glfwSwapBuffers(window);
     }
 
