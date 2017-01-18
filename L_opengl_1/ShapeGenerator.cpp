7c86fabbb61db23d8baf70346324e713d684dd46
DenisDubinin <dadubinin@gmail.com>
2017-01-18 17:35:41 +0100

    light positions

--- L_opengl_1/ShapeGenerator.cpp
+++ L_opengl_1/ShapeGenerator.cpp
@@ -177,9 +177,9 @@ ObjectsData ShapeGenerator::getLights()
     ObjectsData ret;
     vec3 positions[] = {
         vec3( 0.7f,  1.2f,  2.0f),
-        vec3( 2.3f,  2.3f, -4.0f),
-        vec3( 4.0f,  0.0f, -5.0f),
-        vec3( 0.0f,  0.0f, -3.0f)
+        vec3(+4.0f,  0.5f, -3.0f),
+        vec3(-0.0f,  2.5f, -3.0f),
+        vec3(-6.0f,  0.5f, -3.0f)
     };
     int QTY = sizeof(positions);
     
