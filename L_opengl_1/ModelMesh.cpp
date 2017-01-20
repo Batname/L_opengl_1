29f682c6745d2b0365c49785076ab6623369eb21
DenisDubinin <dadubinin@gmail.com>
2017-01-20 07:13:16 +0100

    model mesh1

--- L_opengl_1/ModelMesh.cpp
+++ L_opengl_1/ModelMesh.cpp
@@ -5,31 +5,31 @@ using namespace std;
 ModelMesh::ModelMesh(vector<OriginVertex> vertices, vector<GLuint> indices, vector<OriginTexture> textures) :
     vertices(vertices), indices(indices), textures(textures)
 {
-    /* --- gen buffers --- */
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     glGenBuffers(1, &EBO);
     
-    /* --- bind buffers --- */
     glBindVertexArray(VAO);
+    
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(OriginVertex), &vertices[0], GL_STATIC_DRAW);
     
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ARRAY_BUFFER, indices.size() * sizeof(GLuint), &indices[0], GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLuint), &indices[0], GL_STATIC_DRAW);
     
-    /* --- vertex positions --- */
+    // vertex positions
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(OriginVertex), (GLvoid*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(OriginVertex), (GLvoid *)0);
     
-    /* --- vertex normals --- */
+    // vertex normals
     glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(OriginVertex), (GLvoid*)offsetof(OriginVertex, normal));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(OriginVertex), (GLvoid *)offsetof(OriginVertex, normal));
     
-    
-    /* --- texture normals --- */
+    // vertex texture coordinates
     glEnableVertexAttribArray(2);
-    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(OriginVertex), (GLvoid*)offsetof(OriginVertex, texCoords));
+    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(OriginVertex), (GLvoid *)offsetof(OriginVertex, texCoords));
+    
+    glBindVertexArray(0);
 }
 
 void ModelMesh::draw(class ShaderLoader shader) const
